128b51b5ee437149d250c6115e55b9154bb2463a
Greg Rychlewski <greg.rychlewski@gmail.com>
2023-02-20 09:09:46 -0500

    don't use app config

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,8 +1,17 @@
 Code.require_file("./support/migration.exs", __DIR__)
 
 # Ensure test database is created and the migrations have run
+Application.put_env(:closure_table, CTE.Repo,
+  pool: Ecto.Adapters.SQL.Sandbox,
+  database: "ct_ecto_test",
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost",
+  log: false
+)
+
 CTE.Repo.__adapter__().storage_up(CTE.Repo.config())
 {:ok, _} = CTE.Repo.start_link()
-Ecto.Migrator.up(CTE.Repo, 0, CTE.Migration, log: false)
+Ecto.Migrator.up(CTE.Repo, 0, CTE.Migration)
 
 ExUnit.start()
